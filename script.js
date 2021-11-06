a9f9ab043572c0e5789409c6abe87eee2fa047a6
Nick Paduchowski <82173102+nick-paduchowski@users.noreply.github.com>
2021-11-06 13:32:38 -0400

    Third commit - added more code to script.js

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-let firstSection = [];
-let secondSection = [];
+let firstSection = ['You can ', 'You should ', 'Never give up!'];
+let secondSet = ['achieve it!', 'get the job!', 'do it!'];
+let thirdSet = ['shoot for the stars!', 'never give up!', 'believe in yourself!'];
 
 const displayMessage = () => {
     console.log("Welcome to the Inspirational Message Generator\nToday's inspiration message is: ");
+    let randMessage = firstSection[Math.floor(Math.random(firstSection.length))];
+    if (randMessage === randMessage[0]){
+        console.log(`${randMessage} ${secondSet[Math.floor(Math.random(secondSet.length))]}`);
+    } else if (randMessage === randMessage[1]) {
+        console.log(`${randMessage}${thirdSet[Math.floor(Math.random(thirdSet.length))]}`)
+    } else {
+        console.log(randMessage);
+    }
 }
+
+displayMessage();

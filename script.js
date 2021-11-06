129ea03cd5103027235457781129024ade2f1446
Nick Paduchowski <82173102+nick-paduchowski@users.noreply.github.com>
2021-11-05 22:17:01 -0400

    Second commit - added script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+let firstSection = [];
+let secondSection = [];
+
+const displayMessage = () => {
+    console.log("Welcome to the Inspirational Message Generator\nToday's inspiration message is: ");
+}

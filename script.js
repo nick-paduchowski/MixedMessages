fc26a9f0ea208c14ba50b6e0f6c3e58045970093
Nick Paduchowski <82173102+nick-paduchowski@users.noreply.github.com>
2021-11-06 13:34:18 -0400

    Fourth commit - fixed bug

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ let thirdSet = ['shoot for the stars!', 'never give up!', 'believe in yourself!'
 const displayMessage = () => {
     console.log("Welcome to the Inspirational Message Generator\nToday's inspiration message is: ");
     let randMessage = firstSection[Math.floor(Math.random(firstSection.length))];
-    if (randMessage === randMessage[0]){
-        console.log(`${randMessage} ${secondSet[Math.floor(Math.random(secondSet.length))]}`);
-    } else if (randMessage === randMessage[1]) {
+    if (randMessage === 'You can '){
+        console.log(`${randMessage}${secondSet[Math.floor(Math.random(secondSet.length))]}`);
+    } else if (randMessage === 'You should ') {
         console.log(`${randMessage}${thirdSet[Math.floor(Math.random(thirdSet.length))]}`)
     } else {
         console.log(randMessage);
